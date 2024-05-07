5d9115b56c5bc8aff315517dbc2ad31bab178516
Danny Guinther <dguinther@neuralmagic.com>
2024-05-07 13:14:41 -0400

    Install dependencies before running nm-vllm-production-monitoring chart tests

--- tests/charts/nm_vllm_production_monitoring/conftest.py
+++ tests/charts/nm_vllm_production_monitoring/conftest.py
@@ -1,7 +1,22 @@
-from tests.test_helpers import make_chart_fixtures
+import pytest
+from pytest_helm_templates import HelmRunner
+
+from tests.test_helpers import charts_path, make_chart_fixtures
+
+
+CHART_DIR_NAME = "nm-vllm-production-monitoring"
+
+
+# Override helm_runner fixture so we can use it as a hook to ensure chart
+# dependencies are installed.
+@pytest.fixture(scope="module")
+def helm_runner() -> HelmRunner:
+    helm_runner = HelmRunner(cwd=charts_path())
+    helm_runner.dependency_update(chart=CHART_DIR_NAME)
+    return helm_runner
 
 
 make_chart_fixtures(
-    chart_dir_name="nm-vllm-production-monitoring",
+    chart_dir_name=CHART_DIR_NAME,
     conftest_globals=globals(),
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from pytest_helm_templates import HelmRunner
 from tests.test_helpers import charts_path
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def helm_runner() -> HelmRunner:
     return HelmRunner(cwd=charts_path())
