d42a7cf8af51cd24f0238ebb0d4972cb5428525f
Danny Guinther <dguinther@neuralmagic.com>
2024-05-08 15:20:40 -0400

    Make grafana dashboards chart able to take grafana values (#12)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,20 @@
+from typing import Dict
+
 import pytest
 from pytest_helm_templates import HelmRunner
 
 from tests.test_helpers import charts_path
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def helm_runner() -> HelmRunner:
     return HelmRunner(cwd=charts_path())
+
+
+@pytest.fixture(scope="session")
+def grafana_default_values(helm_runner: HelmRunner) -> Dict:
+    return helm_runner.values(
+        chart="grafana",
+        repo="https://grafana.github.io/helm-charts",
+        version="7.3.9",
+    )
