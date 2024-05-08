e12c1e0aaec6fa09c9326afd708b3499f8ab1bca
dalthecow <dalcowboiz@gmail.com>
2024-05-08 13:48:53 -0400

    add tests for configurable values, refactor datasource value structure

--- tests/charts/nm_vllm_production_monitoring/test_config_map.py
+++ tests/charts/nm_vllm_production_monitoring/test_config_map.py
@@ -1,7 +1,7 @@
 from typing import Dict, Optional
 
 from pytest_helm_templates import HelmRunner
-
+import yaml
 
 def test_grafana_datasources_label_and_label_value_can_be_configured(
     helm_runner: HelmRunner,
@@ -66,6 +66,86 @@ def test_grafana_datasources_label_and_label_value_defaults_match_grafana_defaul
 
     labels = subject["metadata"]["labels"]
     assert labels[label] == label_value
+
+
+def test_datasource_http_method_can_be_configured(
+    helm_runner: HelmRunner,
+) -> None:
+    http_method = "POST"
+    subject = render_subject(
+        helm_runner=helm_runner,
+        values={
+            "datasource": {
+                "httpMethod": http_method
+            },
+        }
+    )
+    datasource_yaml_unparsed = subject["data"]["datasource.yaml"]
+    datasource_yaml = yaml.safe_load(datasource_yaml_unparsed)
+    datasources = datasource_yaml["datasources"]
+    for datasource in datasources:
+        actual_http_method = datasource["jsonData"]["httpMethod"]
+        assert actual_http_method == http_method
+
+
+def test_datasource_scrape_internal_can_be_configured(
+    helm_runner: HelmRunner,
+) -> None:
+    scrape_interval = "POST"
+    subject = render_subject(
+        helm_runner=helm_runner,
+        values={
+            "datasource": {
+                "scrapeInterval": scrape_interval
+            },
+        }
+    )
+    datasource_yaml_unparsed = subject["data"]["datasource.yaml"]
+    datasource_yaml = yaml.safe_load(datasource_yaml_unparsed)
+    datasources = datasource_yaml["datasources"]
+    for datasource in datasources:
+        actual_scrape_interval = datasource["jsonData"]["timeInterval"]
+        assert actual_scrape_interval == scrape_interval
+
+
+def test_datasource_uid_can_be_configured(
+    helm_runner: HelmRunner,
+) -> None:
+    uid = "POST"
+    subject = render_subject(
+        helm_runner=helm_runner,
+        values={
+            "datasource": {
+                "uid": uid
+            },
+        }
+    )
+    datasource_yaml_unparsed = subject["data"]["datasource.yaml"]
+    datasource_yaml = yaml.safe_load(datasource_yaml_unparsed)
+    datasources = datasource_yaml["datasources"]
+    for datasource in datasources:
+        actual_uid = datasource["uid"]
+        assert actual_uid == uid
+
+
+def test_datasource_url_can_be_configured(
+    helm_runner: HelmRunner,
+) -> None:
+    url = "POST"
+    subject = render_subject(
+        helm_runner=helm_runner,
+        values={
+            "datasource": {
+                "url": url
+            },
+        }
+    )
+    datasource_yaml_unparsed = subject["data"]["datasource.yaml"]
+    datasource_yaml = yaml.safe_load(datasource_yaml_unparsed)
+    datasources = datasource_yaml["datasources"]
+    for datasource in datasources:
+        actual_url = datasource["url"]
+        assert actual_url == url
 
 
 def render_subject(
